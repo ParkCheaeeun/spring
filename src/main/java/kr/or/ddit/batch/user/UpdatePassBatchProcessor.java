d32f7f120d26d11eb8a3822c04d010a282ef3598
ParkCheaeeun <ParkCheaeeun.codms121@gmail.com>
2019-09-26 16:55:22 +0900

    spring batch 	. spring-mybatis 모듈 적용 	. 사용 비밀번호 암호화 배치

---
+++ src/main/java/kr/or/ddit/batch/user/UpdatePassBatchProcessor.java
@@ -0,0 +1,19 @@
+package kr.or.ddit.batch.user;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import kr.or.ddit.encrypt.kisa.sha256.KISA_SHA256;
+import kr.or.ddit.user.model.User;
+
+public class UpdatePassBatchProcessor implements ItemProcessor<User, User>{
+
+	public UpdatePassBatchProcessor() {
+	}
+
+	@Override
+	public User process(User item) throws Exception {
+		item.setPass(KISA_SHA256.encrypt(item.getPass()));
+		return item;
+	}
+	
+}
